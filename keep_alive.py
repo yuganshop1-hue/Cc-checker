c4cfe425eada4d11f7ce8a8de9f3226ca11329fb
yuganshop1-hue <yuganshop1@gmail.com>
2026-09-19 00:50:35 +0530

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is online!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
